0ffc121f5cb3351c612e121d2c892fcd1d614f91
FatlumIbishi <fatlum.ibishi@gmail.com>
2020-01-22 11:29:07 +0100

    Images

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,6 +9,7 @@ using Windows.Media.SpeechSynthesis;
 using System.Linq;
 using System.ComponentModel;
 using project_ramverket.Views;
+using Windows.UI.Popups;
 
 namespace project_ramverket
 {
@@ -26,13 +27,31 @@ namespace project_ramverket
 
         public class BaseItem : NotifyPropertyChanged
         {
-            string _header = string.Empty;
+            string _header = "Welcome to FunFacts!";
+            string _image = "Assets/cat-whiskers-kitty-tabby-20787.jpg";
+            string _selected = "";
             public string HeaderName
             {
                 get { return _header; }
                 set
                 {
                     _header = value; OnPropertyRaised("HeaderName");
+                }
+            }
+            public string HeaderImage
+            {
+                get { return _image; }
+                set
+                {
+                    _image = value; OnPropertyRaised("HeaderImage");
+                }
+            }
+            public string SelectedItem
+            {
+                get { return _selected; }
+                set
+                {
+                    _selected = value;
                 }
             }
         }
@@ -50,10 +69,14 @@ namespace project_ramverket
             switch (item.Tag)
             {
                 case "catFact":
+                    BaseHeader.SelectedItem = "cat";
                     ContentFrame.Navigate(typeof(CatView));
+                    BaseHeader.HeaderImage = "Assets/cat-whiskers-kitty-tabby-20787.jpg";
                     break;
                 case "programmingJoke":
+                    BaseHeader.SelectedItem = "dev";
                     ContentFrame.Navigate(typeof(ProgrammerView));
+                    BaseHeader.HeaderImage = "Assets/gray-laptop-computer-showing-html-codes-in-shallow-focus-160107.jpg";
                     break;
             }
         }
